92ceaeedc49e060a7be49246ba031ca512b49431
dolgun0118 <zipo0309@gmail.com>
2024-02-29 17:17:20 +0900

    [New Feature]: ActionButton

--- src/app/(afterLogin)/home/layout.tsx
+++
@@ -1,5 +0,0 @@
-const Layout = async ({ children }: { children: React.ReactNode }) => {
-  return <div>홈페이지 Layout{children}</div>;
-};
-
-export default Layout;
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
-// import styles from "./page.module.css";
+import Tab from "./_component/Tab";
+import PostForm from "./_component/PostForm";
+import Post from "../_component/Post";
+import TabProvider from "./_component/TabProvider";
+import style from "./home.module.css";
 
 export default function Home() {
-  return <main>페이지</main>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        {[...new Array(10)].map((_, idx) => (
+          <Post key={idx} />
+        ))}
+      </TabProvider>
+    </main>
+  );
 }
